7546548d4dbc4e4eb1ff17d6d6636297e2f48bbc
H.Tnacheri <hicham.tnacheri@epitech.eu>
2021-08-03 01:38:53 +0200

    [ADD] arguments gestion / parsing + error handling

--- EventPackage/eventpackage.py
+++ EventPackage/eventpackage.py
@@ -5,6 +5,22 @@ from sqlite3 import Error
 
 class DatetimeEventStore():
     dbconnection = None
+    dbcursor = None
 
     def __init__(self):
+        try: 
+            temp = open("eventdb.db")
+            temp.close()
+            self.dbconnection = sqlite3.connect("eventdb.db")
+            self.dbcursor = self.dbconnection.cursor()
+        except IOError:
+            print("\"eventdb.db\" doesn't exist.")
+
+    def store_event(self, at, data):
+        dateStr = None
+        if (at == None or data == None or type(at != datetime)):
+            print("One or two of the parameters is null / Datetime format isn't good")
+        dateStr = at.strftime("%d-%b-%Y")
+        print(dateStr)
+        print(data)
         
